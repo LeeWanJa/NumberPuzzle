6a90801a49d1c734d1a5124d9500c5b5c23d4050
이재완 <jjjkuul@naver.com>
2025-12-05 23:42:42 +0900

    숫자 퍼즐 구현 완료!
    * Numbers.java - 숫자 리스트의 상태를 조회하고 변화시킬 수 있는 클래스 만듦
    * PuzzleMain.java - 입력 문자를 검증하는 static 메서드 checkValues를 만듦
    * 기능을 하나하나 구현하며 커밋을 자주 했어야 하는데 그러지 못함
    * 다음 문제에서는 기능을 하나씩 만들며 커밋할 예정

---
+++ Puzzle/src/Numbers.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Numbers {
+    private ArrayList<Integer> numbers;
+
+    public Numbers() {
+        ArrayList<Integer> temp = new ArrayList<>();
+        for(int i = 1; i <= 8; i++)
+            temp.add(i);
+
+        Collections.shuffle(temp);
+
+        numbers = temp;
+    }
+
+    // Numbers의 멤버가 정렬되어 있는가 체크
+    public boolean isSort() {
+        for(int i = 1; i <= 8; i++)
+            if(i != this.numbers.get(i - 1))
+                return false;
+
+        return true;
+    }
+
+    // swap 메서드
+    public void swap(int first, int second) {
+        int first_idx = -1, sec_idx = -1;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) == first)
+                first_idx = i;
+
+            if (numbers.get(i) == second)
+                sec_idx = i;
+        }
+
+        int tmp = numbers.get(first_idx);
+        numbers.set(first_idx, numbers.get(sec_idx));
+        numbers.set(sec_idx, tmp);
+    }
+
+    // numbers의 목록들 출력
+    @Override
+    public String toString() {
+        return this.numbers.toString();
+    }
+}
--- Puzzle/src/PuzzleMain.java
+++ Puzzle/src/PuzzleMain.java
@@ -1,5 +1,64 @@
+import java.util.Scanner;
+
 public class PuzzleMain {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("간단 숫자 퍼즐");
+
+        int turn = 1;
+        int first, second;
+        Numbers numbers = new Numbers();
+        Scanner scan = new Scanner(System.in);
+
+        // 퍼즐 시작!
+        System.out.println("Turn " + turn);
+        System.out.println(numbers.toString());
+        while(!numbers.isSort()){
+            // 정수 입력
+            System.out.println("교환할 두 숫자를 입력>");
+            String[] inputs = scan.nextLine().split(",");
+
+            // 검증
+            if(!checkValues(inputs)){
+                System.out.println("잘못 입력하셨습니다. 다시 입력해 주세요");
+                continue;
+            }
+
+            first = Integer.parseInt(inputs[0]);
+            second = Integer.parseInt(inputs[1].trim());
+
+            // 두 수 swap
+            numbers.swap(first, second);
+
+            turn++;
+            System.out.println("Turn " + turn);
+            System.out.println(numbers.toString() + "\n");
+        }
+        System.out.println("축하합니다! " + turn + "턴만에 퍼즐을 완성하셨습니다!");
+    }
+
+    private static boolean checkValues(String[] arr) {
+        // 입력이 두 개가 아닌 경우
+        if(arr.length != 2)
+            return false;
+        arr[1] = arr[1].trim();
+
+        // 시작에 공백이 있는 경우
+        if(arr[0].contains(" "))
+            return false;
+
+        // 둘 중 하나라도 정수 변환이 안 되는 경우
+        int first, second;
+        try {
+            first = Integer.parseInt(arr[0]);
+            second = Integer.parseInt(arr[1]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        // 범위 확인
+        if(!((1 <= first && first <= 8) && (1 <= second && second <= 8)))
+            return false;
+
+        return true;
     }
 }
