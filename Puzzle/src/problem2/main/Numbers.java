c51bdcfb37119b0c4ed3e2ba2285893cab6fdcff
이재완 <jjjkuul@naver.com>
2025-12-06 17:11:05 +0900

    퍼즐 유효성 검증 코드 작성
    * puzzle_validation_check - inversion의 개수와 0(빈칸)의 위치로 퍼즐이 풀릴 수 있는 퍼즐인지 검증하는 테스트 코드 메서드
    * checkNumbers - 위의 테스트 코드 로직을 그대로 다시 옮겨 적음
    * checkEvenOdd - 홀/짝 검증

--- Puzzle/src/problem2/main/Numbers.java
+++ Puzzle/src/problem2/main/Numbers.java
@@ -20,27 +20,60 @@ public class Numbers {
         // 검증
         while(true){
             Collections.shuffle(temp);
-
             if(checkNumbers(temp))
                 break;
         }
 
         int idx = 0;
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 4; j++) {
+        for(int i = 0; i < 4; i++)
+            for(int j = 0; j < 4; j++)
                 newNumbers[i][j] = temp.get(idx++);
-            }
-        }
 
         return new Numbers(newNumbers);
     }
 
     // 유효한 퍼즐인지 검증
     private static boolean checkNumbers(ArrayList<Integer> numbers){
-        return true;
+        // https://www.geeksforgeeks.org/dsa/check-instance-15-puzzle-solvable/ 참고
+        
+        // inversion의 합계
+        int idx = -1;
+        int inversion = 0;
+        for(int i = 0; i <= 15; i++){
+            int num = numbers.get(i);
+
+            if(num == 0){
+                idx = i;
+                continue;
+            }
+
+            for(int j = i + 1; j <= 15; j++){
+                if(num > numbers.get(j) && numbers.get(j) != 0)
+                    inversion++;
+            }
+        }
+
+        // 0은 아래를 기준으로 홀수 열에 위치했는가, 짝수 열에 위치했는가?
+        boolean idx_row = checkEvenOdd(idx / 4);
+        if(idx_row && !checkEvenOdd(inversion)) // 짝수열에 위치한 경우 inversion은 홀수여야 함
+            return true;
+
+        if(!idx_row && checkEvenOdd(inversion)) // 홀수열에 위치한 경우 inversion은 짝수여야 함
+            return true;
+
+        return false;
     }
 
-    // 2차원 배열 출력
+    // 짝수 홀수 체크
+    public static boolean checkEvenOdd(int num){
+        return num % 2 == 0;
+    }
+
+    public int[][] getNumbers() {
+        return numbers;
+    }
+
+    // 2차원 배열 문자열로 출력
     @Override
     public String toString() {
         String result = "";
--- Puzzle/src/problem2/test/NumbersTest.java
+++ Puzzle/src/problem2/test/NumbersTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import problem2.main.Numbers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class NumbersTest {
 
@@ -17,13 +19,16 @@ public class NumbersTest {
     }
 
     @Test
-    void puzzle_validation_checkInversionNum() {
-        int[] arr = {13, 2, 10, 3, 1, 12, 8, 4, 5, 0, 9, 6, 15, 14, 11, 7};
+    void puzzle_validation_check() {
+        // int[] arr = {13, 2, 10, 3, 1, 12, 8, 4, 5, 0, 9, 6, 15, 14, 11, 7};
+        // int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 14, 0};
+        // int[] arr = {6, 13, 7, 10, 8, 9, 11, 0, 15, 2, 12, 5, 14, 3, 1, 4};
+        int[] arr = {3, 9, 1, 15, 14, 11, 4, 6, 13, 0, 10, 12, 2, 7, 8, 5};
 
         // inversion의 합계
         int idx = -1;
         int inversion = 0;
-        for(int i = 0; i < 15; i++){
+        for(int i = 0; i <= 15; i++){
             int num = arr[i];
 
             if(num == 0){
@@ -37,8 +42,29 @@ public class NumbersTest {
             }
         }
 
-        int expectedInversion = 41;
-
+        //int expectedInversion = 41;
+        //int expectedInversion = 1;
+        //int expectedInversion = 62;
+        int expectedInversion = 56;
+        System.out.println(inversion + " | " + expectedInversion);
         Assertions.assertEquals(expectedInversion, inversion);
+
+
+        System.out.println("0의 위치는? " + idx);
+        // 짝수 홀수 검증
+        boolean idx_row = (idx / 4) % 2 == 0;
+        boolean resultBool = false;
+        if(idx_row && !(inversion % 2 == 0)) // 짝수인 경우
+            resultBool = true;
+
+        if(!idx_row && (inversion % 2 == 0)) // 홀수인 경우
+            resultBool = true;
+
+        //boolean expectedBool = true;
+        //boolean expectedBool = false;
+        //boolean expectedBool = true;
+        boolean expectedBool = false;
+        System.out.println(resultBool + " | " + expectedBool);
+        Assertions.assertEquals(expectedBool, resultBool);
     }
 }
