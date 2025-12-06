0fb9680990d84f486eb8e99fe70cebcd45a63a03
이재완 <jjjkuul@naver.com>
2025-12-06 14:01:24 +0900

    랜덤숫자 생성 로직
    * 정적 팩토리 메서드 패턴으로 숫자 퍼즐 초기값을 생성
    * createNumbers 메서드는 4 * 4 크기의 랜덤한 값들이 할당된 이차원 배열을 생성하고 Numbers 객체를 반환
    * Numbers 객체 생성은 오직 createNumbers를 통해서 가능

--- Puzzle/src/problem2/main/Numbers.java
+++ Puzzle/src/problem2/main/Numbers.java
@@ -1,4 +1,57 @@
 package problem2.main;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Numbers {
+    int[][] numbers;
+
+    private Numbers(int[][] numbers) {
+        this.numbers = numbers;
+    }
+
+    // 1 ~ 15의 퍼즐 생성
+    public static Numbers createNumbers(){
+        int[][] newNumbers = new int[4][4];
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        for(int i = 0; i < 16; i++)
+            temp.add(i);
+
+        // 검증
+        Collections.shuffle(temp);
+
+        int idx = 0;
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 4; j++) {
+                newNumbers[i][j] = temp.get(idx++);
+            }
+        }
+
+        return new Numbers(newNumbers);
+    }
+
+    // 유효한 퍼즐인지 검증
+    private boolean checkNumbers(){
+        return false;
+    }
+
+    // 2차원 배열 출력
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (numbers[i][j] == 0){
+                    result += "[  ]";
+                    continue;
+                }
+
+                String tmp = String.format("[%2d]",  numbers[i][j]);
+                result += tmp;
+            }
+            result += "\n";
+        }
+
+        return result;
+    }
 }
--- Puzzle/src/problem2/test/NumbersTest.java
+++ Puzzle/src/problem2/test/NumbersTest.java
@@ -1,13 +1,16 @@
 package problem2.test;
 
 import org.junit.jupiter.api.Test;
+import problem2.main.Numbers;
 
 import java.util.ArrayList;
 
 public class NumbersTest {
 
     @Test
-    void printRandomNumbers() {
+    void print_numbers() {
+        Numbers numbers = Numbers.createNumbers();
 
+        System.out.println(numbers.toString());
     }
 }
