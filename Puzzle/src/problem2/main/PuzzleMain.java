8d54b8a7d8c0e9c04e1a8df65b1033d701470549
이재완 <jjjkuul@naver.com>
2025-12-06 17:40:56 +0900

    사용자 입력 검증 메서드 checkInput 만듦

--- Puzzle/src/problem2/main/PuzzleMain.java
+++ Puzzle/src/problem2/main/PuzzleMain.java
@@ -1,7 +1,55 @@
 package problem2.main;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class PuzzleMain {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Numbers numbers = Numbers.createNumbers();
+        Scanner scan = new Scanner(System.in);
+        int turn = 1;
+        int inputValue;
+
+        System.out.println("재밌는 15 퍼즐!");
+        System.out.println("Turn " + turn);
+        while(true){
+            System.out.println(numbers.toString());
+            System.out.print("숫자 입력>");
+            // 입력 유효성 검증
+            String input = scan.nextLine();
+            if((inputValue = checkInput(input)) == -1)
+                continue;
+
+//            // swap
+//            if(!numbers.swap(inputValue)){
+//                System.out.println("빈칸에 '상/하/좌/우'로 인접한 숫자를 입력해주세요!");
+//                continue;
+//            }
+//
+//            // 정렬 검증
+//            if(numbers.isSort()){
+//                System.out.println("축하합니다! " + turn + "턴만에 퍼즐을 완성하셨습니다!");
+//                break;
+//            }
+            System.out.println("Turn " + turn);
+        }
+    }
+
+    public static int checkInput(String input){
+        String str = input.trim();
+        int result;
+        try {
+            result = Integer.parseInt(str);
+        }  catch (NumberFormatException e) {
+            System.out.println("유효하지 않은 입력입니다!");
+            return -1;
+        }
+
+        if(!(1 <= result && result <= 15)){
+            System.out.println("유효하지 않은 입력입니다!");
+            return -1;
+        }
+
+        return result;
     }
 }
--- Puzzle/src/problem2/test/NumbersTest.java
+++ Puzzle/src/problem2/test/NumbersTest.java
@@ -23,7 +23,8 @@ public class NumbersTest {
         // int[] arr = {13, 2, 10, 3, 1, 12, 8, 4, 5, 0, 9, 6, 15, 14, 11, 7};
         // int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 14, 0};
         // int[] arr = {6, 13, 7, 10, 8, 9, 11, 0, 15, 2, 12, 5, 14, 3, 1, 4};
-        int[] arr = {3, 9, 1, 15, 14, 11, 4, 6, 13, 0, 10, 12, 2, 7, 8, 5};
+        // int[] arr = {3, 9, 1, 15, 14, 11, 4, 6, 13, 0, 10, 12, 2, 7, 8, 5};
+        int[] arr = {12, 8, 11, 10, 2, 5, 14, 15, 6, 1, 0, 4, 13, 9, 7, 3};
 
         // inversion의 합계
         int idx = -1;
@@ -45,11 +46,12 @@ public class NumbersTest {
         //int expectedInversion = 41;
         //int expectedInversion = 1;
         //int expectedInversion = 62;
-        int expectedInversion = 56;
+        //int expectedInversion = 56;
+        int expectedInversion = 63;
         System.out.println(inversion + " | " + expectedInversion);
         Assertions.assertEquals(expectedInversion, inversion);
 
-
+        System.out.println(Arrays.toString(arr));
         System.out.println("0의 위치는? " + idx);
         // 짝수 홀수 검증
         boolean idx_row = (idx / 4) % 2 == 0;
@@ -63,7 +65,9 @@ public class NumbersTest {
         //boolean expectedBool = true;
         //boolean expectedBool = false;
         //boolean expectedBool = true;
-        boolean expectedBool = false;
+        //boolean expectedBool = false;
+        boolean expectedBool = true;
+
         System.out.println(resultBool + " | " + expectedBool);
         Assertions.assertEquals(expectedBool, resultBool);
     }
